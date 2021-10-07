99924d342f6a237c46b760dd29900a7c05c865b0
Asmenys <76791232+Asmenys@users.noreply.github.com>
2021-10-07 16:28:49 +0300

    works with duplicate numbers

--- mergesort.rb
+++ mergesort.rb
@@ -1,25 +1,30 @@
 # frozen_string_literal: true
 
+require 'pry-byebug'
 # a recursive sorting method
 def mergesort(array)
   results = []
   if array.length > 1
-    array_B = mergesort(array[0...array.length / 2])
-    array_C = mergesort(array[array.length / 2...array.length])
+    array_b = mergesort(array[0...array.length / 2])
+    array_c = mergesort(array[array.length / 2...array.length])
     i1 = 0
     i2 = 0
-    while results.length < array_B.length + array_C.length
-      if array_C[i2].nil?
-        results << array_B[i1]
+    while results.length < array_b.length + array_c.length
+      if array_c[i2].nil?
+        results << array_b[i1]
         i1 += 1
-      elsif array_B[i1].nil?
-        results << array_C[i2]
+      elsif array_b[i1].nil?
+        results << array_c[i2]
         i2 += 1
-      elsif array_B[i1] < array_C[i2]
-        results << array_B[i1]
+      elsif array_b[i1] < array_c[i2]
+        results << array_b[i1]
         i1 += 1
-      elsif array_C[i2] < array_B[i1]
-        results << array_C[i2]
+      elsif array_c[i2] < array_b[i1]
+        results << array_c[i2]
+        i2 += 1
+      elsif array_c[i2] == array_b[i1]
+        results << array_b[i1] << array_c[i2]
+        i1 += 1
         i2 += 1
       end
     end
@@ -27,3 +32,5 @@ def mergesort(array)
   end
   results
 end
+binding.pry
+test = 'test'
